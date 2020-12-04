eca3182e114fc2213fcaee2d2338480e3ca801ea
Luan Matheus <42500368+LuanSatiro@users.noreply.github.com>
2020-12-04 00:29:35 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 DEBUG = True
 
-SQLALCHEMY_DATABASE_URI ='mysql+pymysql://b9a48622c992ac:473a364b@us-cdbr-east-02.cleardb.com/heroku_60945ed48fc1c50?reconnect=true'
+SQLALCHEMY_DATABASE_URI ='mysql//b9a48622c992ac:473a364b@us-cdbr-east-02.cleardb.com/heroku_60945ed48fc1c50?reconnect=true'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 FLASK_ADMIN_SWATCH = 'cerulean'
 SECRET_KEY='thisascretkey'
