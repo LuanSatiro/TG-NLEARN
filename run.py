d4207f3751d7cd16149de206ec59d55aacbc0f0a
Luan Matheus <42500368+LuanSatiro@users.noreply.github.com>
2020-12-04 01:17:34 -0300

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,5 @@
-import app
-import os
+from app import manager
 
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
-    
+
+if _name_ == "_main_":
+    manager.run()
