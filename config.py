222f321bf9a5c08107c63caab139c8b36aa009bf
LuanSatiro <satiro_underground@hotmail.com>
2020-12-08 17:20:17 -0300

    att

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 DEBUG = True
-<<<<<<< HEAD
+
 port = 4000
 
 SQLALCHEMY_DATABASE_URI ='mysql+pymysql://root:@maxter@m4@localhost/nlearn'
-=======
-SQLALCHEMY_DATABASE_URI = 'mysql://b9a48622c992ac:473a364b@us-cdbr-east-02.cleardb.com/heroku_60945ed48fc1c50'
->>>>>>> fb3ff0d66fe48fbf3612ccdfb347727cac212b92
+
+
+
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 FLASK_ADMIN_SWATCH = 'cerulean'
 SECRET_KEY='thisascretkey'
